60efa6c70b9a258d546cc0abb48a2fa8a4e9602c
kudotaka <kudotaka@aurora.ocn.ne.jp>
2024-12-11 13:26:02 +0000

    Change commit

--- DataPosting6/Program.cs
+++ DataPosting6/Program.cs
@@ -64,6 +64,7 @@ public class DataPortingApp : ConsoleAppBase
     readonly IOptions<MyConfig> config;
 
     Dictionary<MyTypes, List<MyDefinition>> dicMyDefinition = new Dictionary<MyTypes, List<MyDefinition>>();
+    Dictionary<string, MyCable> dicMyCable = new Dictionary<string, MyCable>();
 
     public DataPortingApp(ILogger<DataPortingApp> logger,IOptions<MyConfig> config)
     {
@@ -72,7 +73,7 @@ public class DataPortingApp : ConsoleAppBase
     }
 
 //    [Command("")]
-    public void Porting(string orignal, string format, string outpath)
+    public void Porting(string orignal, string format, string cable, string outpath)
     {
 //== start
         logger.ZLogInformation($"==== tool {getMyFileVersion()} ====");
@@ -86,12 +87,20 @@ public class DataPortingApp : ConsoleAppBase
             logger.ZLogError($"[NG] エクセルファイルが見つかりません{format}");
             return;
         }
+        if (!File.Exists(cable))
+        {
+            logger.ZLogError($"[NG] エクセルファイルが見つかりません{cable}");
+            return;
+        }
 
         string orignalTypeCell = config.Value.OrignalTypeCell;
         string formatSheetName = config.Value.FormatSheetName;
 
         readDefinition(dicMyDefinition);
         printDefinition(dicMyDefinition);
+
+        readCable(cable, dicMyCable);
+        printCable(dicMyCable);
 
         portingExcel(orignal, orignalTypeCell, format, outpath, dicMyDefinition);
 
@@ -144,6 +153,72 @@ public class DataPortingApp : ConsoleAppBase
         logger.ZLogTrace($"== end print ==");
     }
 
+    private void readCable(string cableExcel, Dictionary<string, MyCable> dic)
+    {
+        string cableRackNameStartWord = config.Value.CableRackNameStartWord;
+        int cableRackNameColumn = config.Value.CableRackNameColumn;
+        int cableDeviceNameColumn = config.Value.CableDeviceNameColumn;
+        int cableDeviceNumberColumn = config.Value.CableDeviceNumberColumn;
+        int cableHostNameColumn = config.Value.CableHostNameColumn;
+
+        FileStream fs = new FileStream(cableExcel, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using XLWorkbook xlWorkbook = new XLWorkbook(fs);
+        IXLWorksheets sheets = xlWorkbook.Worksheets;
+        foreach (IXLWorksheet? sheet in sheets)
+        {
+            if (sheet != null)
+            {
+                int lastUsedRowNumber = sheet.LastRowUsed() == null ? 0 : sheet.LastRowUsed().RowNumber();
+                for (int r = 1; r < lastUsedRowNumber + 1; r++)
+                {
+                    IXLCell cellRackName = sheet.Cell(r, cableRackNameColumn);
+                    if (cellRackName.IsEmpty() == true)
+                    {
+                        // nothing
+                        logger.ZLogTrace($"readCable() IsEmpty! sheet:{sheet.Name} row:{r}");
+                    }
+                    else
+                    {
+                        string rackName = cellRackName.Value.GetText();
+                        if (rackName.StartsWith(cableRackNameStartWord))
+                        {
+                            string tmpDeviceName = sheet.Cell(r, cableDeviceNameColumn).Value.GetText();
+                            string tmpDiviceNumber = sheet.Cell(r, cableDeviceNumberColumn).Value.GetText();
+                            MyCable tmpCable = new MyCable();
+                            tmpCable.rackName = rackName;
+                            tmpCable.deviceNameAndNumber = tmpDeviceName + tmpDiviceNumber;
+                            tmpCable.hostName = sheet.Cell(r, cableHostNameColumn).Value.GetText();
+                            if (dic.ContainsKey(tmpCable.deviceNameAndNumber))
+                            {
+                                logger.ZLogTrace($"readCable() 重複で追加しない rack:{rackName} sheet:{sheet.Name} row:{r}");
+                                continue;
+                            }
+                            else
+                            {
+                                dic.Add(tmpCable.deviceNameAndNumber, tmpCable);
+                            }
+                        }
+                        else
+                        {
+                            logger.ZLogTrace($"readCable() StartsWithに不一致でした rack:{rackName} sheet:{sheet.Name} row:{r}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void printCable(Dictionary<string, MyCable> dic)
+    {
+        logger.ZLogTrace($"== start print ==");
+        foreach (var key in dic.Keys)
+        {
+            var cable = dic[key];
+            logger.ZLogTrace($"キー:{key} デバイス名:{cable.deviceNameAndNumber} ラック名:{cable.rackName} ホスト名:{cable.hostName}");
+        }
+        logger.ZLogTrace($"== end print ==");
+    }
+
     private string convertTypesToReadableTypes(MyTypes types)
     {
         switch (types)
@@ -184,6 +259,7 @@ public class DataPortingApp : ConsoleAppBase
             IXLCell cellColumn = sheetOrignal.Cell(orignalTypeCell);
             logger.ZLogDebug($"cell is Text type. value:{cellColumn.GetValue<string>()}");
             var type = convertWordToType(cellColumn.GetValue<string>());
+/*
             switch (type)
             {
                 case MyTypes.Type6U:
@@ -195,7 +271,7 @@ public class DataPortingApp : ConsoleAppBase
                 default:
                     break;
             }
-
+*/
             foreach (IXLWorksheet? sheetFormat in xlWorkbookFormatExcel.Worksheets)
             {
                     logger.ZLogDebug($"sheetFormat {sheetFormat.Name}");
@@ -204,10 +280,71 @@ public class DataPortingApp : ConsoleAppBase
             // copy sheet
             var formatSheet = xlWorkbookFormatExcel.Worksheet(covertTypeToSheetName(type));
             var portingSheet = formatSheet.CopyTo(sheetOrignal.Name);
+            // 目盛線の非表示
+            portingSheet.SetShowGridLines(false);
             // copy cell
             foreach (var definition in dic[type])
             {
-                portingSheet.Cell(definition.portingCell).Value = sheetOrignal.Cell(definition.orignalCell).Value;
+                string tmpString = sheetOrignal.Cell(definition.orignalCell).Value.ToString();
+                string replaceWord = config.Value.ReplaceWord;
+                string tmpString2 = "";
+                foreach (var word in replaceWord.Split(','))
+                {
+                    tmpString2 = tmpString.Replace(word,"");
+                }
+                logger.ZLogTrace($"cell {tmpString} --> {tmpString2}");
+                portingSheet.Cell(definition.portingCell).SetValue(tmpString2.ToString());
+            }
+
+            // DeviceNameToHostName
+            switch (type)
+            {
+                case MyTypes.Type6U:
+                    string target6UDeviceNameToHostName = config.Value.Target6UDeviceNameToHostName;
+                    foreach (var keyAndValue in target6UDeviceNameToHostName.Split(','))
+                    {
+                        string[] item = keyAndValue.Split('|');
+                        if (portingSheet.Cell(item[0]).IsEmpty())
+                        {
+                            continue;
+                        }
+                        string targetDeviceName = portingSheet.Cell(item[0]).Value.GetText();
+                        if (dicMyCable.ContainsKey(targetDeviceName))
+                        {
+                            MyCable cable = dicMyCable[targetDeviceName];
+                            logger.ZLogTrace($"6U! {item[0]}:{targetDeviceName} add--> {item[1]}:{cable.hostName}");
+                            portingSheet.Cell(item[1]).SetValue(cable.hostName.ToString());
+                        }
+                        else
+                        {
+                            logger.ZLogTrace($"DeviceNameToHostName Not ContainsKey {targetDeviceName}");
+                        }
+                    }
+                    break;
+                case MyTypes.Type14U:
+                    string target14UDeviceNameToHostName = config.Value.Target14UDeviceNameToHostName;
+                    foreach (var keyAndValue in target14UDeviceNameToHostName.Split(','))
+                    {
+                        string[] item = keyAndValue.Split('|');
+                        if (portingSheet.Cell(item[0]).IsEmpty())
+                        {
+                            continue;
+                        }
+                        string targetDeviceName = portingSheet.Cell(item[0]).Value.GetText();
+                        if (dicMyCable.ContainsKey(targetDeviceName))
+                        {
+                            MyCable cable = dicMyCable[targetDeviceName];
+                            logger.ZLogTrace($"14U! {item[0]}:{targetDeviceName} add--> {item[1]}:{cable.hostName}");
+                            portingSheet.Cell(item[1]).SetValue(cable.hostName.ToString());
+                        }
+                        else
+                        {
+                            logger.ZLogTrace($"DeviceNameToHostName Not ContainsKey {targetDeviceName}");
+                        }
+                    }
+                    break;                
+                default:
+                    break;
             }
         }
 
@@ -282,6 +419,14 @@ public class MyConfig
     public string DefinitionWordToType {get; set;} = "";
     public string OrignalTypeCell {get; set;} = "";
     public string FormatSheetName {get; set;} = "";
+    public string CableRackNameStartWord {get; set;} = "";
+    public int CableRackNameColumn {get; set;} = 0;
+    public int CableDeviceNameColumn {get; set;} = 0;
+    public int CableDeviceNumberColumn {get; set;} = 0;
+    public int CableHostNameColumn {get; set;} = 0;
+    public string Target6UDeviceNameToHostName {get; set;} = "";
+    public string Target14UDeviceNameToHostName {get; set;} = "";
+    public string ReplaceWord {get; set;} = "";
 }
 
 public enum MyTypes
@@ -295,4 +440,11 @@ public class MyDefinition
 {
     public string orignalCell { set; get; } = "";
     public string portingCell { set; get; } = "";
+}
+
+public class MyCable
+{
+    public string rackName { set; get; } = "";
+    public string deviceNameAndNumber { set; get; } = "";
+    public string hostName { set; get; } = "";
 }
